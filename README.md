d57a8e28abe737b37d85f2ccc6a71b9030602d51
sveloo <sanjeev.veloo@gmail.com>
2016-11-19 02:58:44 +1100

    Fixed up a couple of issues highlighted by reviewer.

--- README.md
+++ README.md
@@ -8,6 +8,11 @@ Developed for Project 6 of the **Udacity Front-End Web Developer Nanodegree** by
 
 In this project I was given a web-based application that reads RSS feeds. The original developer of this application clearly saw the value in testing, they've already included [Jasmine](http://jasmine.github.io/) and even started writing their first test suite! Unfortunately, they decided to move on to start their own company and we're now left with an application with an incomplete test suite. That's where I came in.
 
+## Installation
+
+1. Fork and download the repo. It has everything you need to get started.
+2. Open index.html
+
 ## Why this Project?
 
 Testing is an important part of the development process and many organizations practice a standard of development known as "test-driven development". This is when developers write tests first, before they ever start developing their application. All the tests initially fail and then they start writing application code to make these tests pass.
--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,94 +1,54 @@
-/* feedreader.js
- *
- * This is the spec file that Jasmine will read and contains
- * all of the tests that will be run against your application.
- */
-
-/* We're placing all of our tests within the $() function,
- * since some of these tests may require DOM elements. We want
- * to ensure they don't run until the DOM is ready.
- */
 $(function() {
-    /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
+
     describe('RSS Feeds', function() {
-        /* This is our first test - it tests to make sure that the
-         * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
-         */
+
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
 
-
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         */
-         it('URLs are not empty', function(){
-            // Loop through the feed and check if the URL string contains 'http'
-            // so that we can check that it's not empty and it's also a valid URL.
+        it('URLs are not empty', function(){
+            // Loop through the feed and check if the URL is defined and string contains 'http'
             for (var i = 0; i < allFeeds.length; i++) {
+                expect(allFeeds[i].url).toBeDefined();
                 expect(allFeeds[i].url).toContain('http');
             }
-         })
+        })
 
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
-         */
-         it('Has a name defined', function(){
+        it('Has a name defined', function(){
             // This loops through the feed to check if the name has a string as a title.
             for (var i = 0; i < allFeeds.length; i++) {
                 expect(allFeeds[i].name).toBeTruthy();
             }
          })
-
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
     describe('The Menu', function() {
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
+
         it('Hidden by default', function() {
             // Checks to see if body has the class of menu-hidden
             expect($('body').hasClass('menu-hidden')).toBe(true);
         })
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
+
         it('Visible on click', function(){
             // triggers the click of the menu icon
-            $('.menu-icon-link').click();
             // check to see if the menu-hidden class has been removed from the body tag
+            $('.menu-icon-link').click();
             expect($('body').hasClass('menu-hidden')).toBe(false);
             // trigger another click of the menu icon to close the menu
+            // check to see if the menu-hidden class has been added back to body tag
             $('.menu-icon-link').click();
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+
         })
     });
 
 
-    /* TODO: Write a new test suite named "Initial Entries" */
     describe('Initial Entries', function(){
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
+
         // load the feed before doing anything
         beforeEach(function(done) {
             loadFeed(0, done);
@@ -102,12 +62,8 @@ $(function() {
     });
 
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
     describe('New Feed Selection', function(){
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-         */
+
         // declaring variables outside the it scope.
         var firstFeed, secondFeed;
 
