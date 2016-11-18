8f305a9cfd791a70d65098643621170c115b63ac
sveloo <sanjeev.veloo@gmail.com>
2016-11-18 22:26:19 +1100

    Done!!!!

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -32,6 +32,8 @@ $(function() {
          * and that the URL is not empty.
          */
          it('URLs are not empty', function(){
+            // Loop through the feed and check if the URL string contains 'http'
+            // so that we can check that it's not empty and it's also a valid URL.
             for (var i = 0; i < allFeeds.length; i++) {
                 expect(allFeeds[i].url).toContain('http');
             }
@@ -43,6 +45,7 @@ $(function() {
          * and that the name is not empty.
          */
          it('Has a name defined', function(){
+            // This loops through the feed to check if the name has a string as a title.
             for (var i = 0; i < allFeeds.length; i++) {
                 expect(allFeeds[i].name).toBeTruthy();
             }
@@ -59,6 +62,7 @@ $(function() {
          * hiding/showing of the menu element.
          */
         it('Hidden by default', function() {
+            // Checks to see if body has the class of menu-hidden
             expect($('body').hasClass('menu-hidden')).toBe(true);
         })
          /* TODO: Write a test that ensures the menu changes
@@ -67,8 +71,12 @@ $(function() {
           * clicked and does it hide when clicked again.
           */
         it('Visible on click', function(){
+            // triggers the click of the menu icon
             $('.menu-icon-link').click();
+            // check to see if the menu-hidden class has been removed from the body tag
             expect($('body').hasClass('menu-hidden')).toBe(false);
+            // trigger another click of the menu icon to close the menu
+            $('.menu-icon-link').click();
         })
     });
 
@@ -81,16 +89,46 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        // load the feed before doing anything
+        beforeEach(function(done) {
+            loadFeed(0, done);
+        });
+
+        it('At least a single .entry element in the .feed container', function(){
+            // check to see if there are entries in the .feed
+            expect($('.feed .entry').length).toBeGreaterThan(0);
+        })
 
     });
 
 
-    /* TODO: Write a new test suite named "New Feed Selection"
+    /* TODO: Write a new test suite named "New Feed Selection" */
+    describe('New Feed Selection', function(){
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
-    describe('New Feed Selection', function(){
+        // declaring variables outside the it scope.
+        var firstFeed, secondFeed;
 
+        // here I load the feed and get the length of characters in the feed
+        beforeEach(function(done) {
+            loadFeed(0, done);
+            // store result in firstFeed variable
+            firstFeed = $('.feed .entry').text().length;
+            console.log('First feed length:' + firstFeed);
+        });
+
+        it('New feed changes content of page', function(done){
+            // here I load the second feed, wait till it's loaded,
+            loadFeed(1, function(){
+                done();
+                // count the number of characters in the second feed
+                secondFeed = $('.feed .entry').text().length;
+                console.log('Second feed length:' + secondFeed);
+            })
+            // the two feeds should have different length of characters
+            expect(firstFeed).not.toEqual(secondFeed);
+        });
     })
 }());
